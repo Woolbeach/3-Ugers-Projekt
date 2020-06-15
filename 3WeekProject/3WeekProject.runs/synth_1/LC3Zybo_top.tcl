ab773bbf13bc4ccdae2cf2475cc4ca3cc75d5899
Woolbeach <christian.ulstrand@gmail.com>
2020-06-15 08:06:12 +0200

    Add files via upload

--- 3WeekProject/3WeekProject.runs/synth_1/LC3Zybo_top.tcl
+++ 3WeekProject/3WeekProject.runs/synth_1/LC3Zybo_top.tcl
@@ -17,6 +17,9 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
+set_param synth.incrementalSynthesisCache {C:/Users/Christian Ulstrand/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-8656-DESKTOP-KRIG1B0/incrSyn}
+set_msg_config -id {Synth 8-256} -limit 10000
+set_msg_config -id {Synth 8-638} -limit 10000
 create_project -in_memory -part xc7z010clg400-1
 
 set_param project.singleFileAddWarning.threshold 0
@@ -30,19 +33,23 @@ set_property board_part digilentinc.com:zybo:part0:2.0 [current_project]
 set_property ip_output_repo {c:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.cache/ip} [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 read_vhdl -library xil_defaultlib {
-  {C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/BtnToggle.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/imports/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/BtnToggle.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/new/CustomTimer.vhd}
   {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/new/Memory.vhd}
-  {C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/debounce.vhd}
-  {C:/Users/Christian Ulstrand/Downloads/LC3Zybo/lc3_wrapper_multiplexers.vhd}
-  {C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/lc3_wrapper_multiplexers.vhd}
-  {C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/lc3_computer.vhd}
-  {C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/lc3_debug.vhd}
-  {C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/student_code.vhd}
-  {C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/zybovios_wrapper.vhd}
-  {C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/top.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/new/UARTReceiver.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/new/UARTTransmitter.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/new/baud_generator.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/imports/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/debounce.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/imports/Christian Ulstrand/Downloads/LC3Zybo/lc3_wrapper_multiplexers.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/imports/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/lc3_wrapper_multiplexers.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/imports/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/lc3_computer.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/imports/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/lc3_debug.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/imports/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/student_code.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/imports/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/zybovios_wrapper.vhd}
+  {C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/imports/LC3Zybo/top.vhd}
 }
-read_edif {{C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/ZyboVIO_SE.ngc}}
-read_edif {{C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/lc3.ngc}}
+read_edif {{C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/imports/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/ZyboVIO_SE.ngc}}
+read_edif {{C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/sources_1/imports/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/3 ugers/LC3/LC3Zybo/lc3.ngc}}
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
 # design are intentionally left as such for best results. Dcp files will be
@@ -51,8 +58,8 @@ read_edif {{C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
-read_xdc {{C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/ZyboVIO.xdc}}
-set_property used_in_implementation false [get_files {{C:/Users/Christian Ulstrand/OneDrive/DTU - IT Elektronik/2. Semester/Digitale Systemer/Software/ZyboVIO.xdc}}]
+read_xdc {{C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/constrs_1/imports/Software/ZyboVIO.xdc}}
+set_property used_in_implementation false [get_files {{C:/Users/Christian Ulstrand/Documents/GitHub/3-Ugers-Projekt/3WeekProject/3WeekProject.srcs/constrs_1/imports/Software/ZyboVIO.xdc}}]
 
 set_param ips.enableIPCacheLiteLoad 0
 close [open __synthesis_is_running__ w]
